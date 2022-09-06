5ec068b462fc096a6154d6d7662826389057a897
TanTheta15 <100551214+TanTheta15@users.noreply.github.com>
2022-09-06 18:38:30 +0530

    Update Critical Connections in a Network leetcode hard question.cpp

--- Critical Connections in a Network leetcode hard question.cpp
+++ Critical Connections in a Network leetcode hard question.cpp
@@ -1,31 +1,31 @@
 unordered_map<int, vector<int> > adj;
 
-    void DFS(int u, vector<int> &disc, vector<int> &low, vector<int> &parent, vector<vector<int> > &bridges){
+    void DFS(int u, int parent, vector<int> &disc, vector<int> &low, vector<vector<int> > &bridges){
         static int time = 0;
         disc[u] = low[u] = time;
         time += 1;
         for(int v : adj[u]){
+            if(v == parent) continue;
             if(disc[v] == -1){ // if node is not visited
-                parent[v] = u;
-                DFS(v, disc, low, parent, bridges);
+                DFS(v, u, disc, low, bridges);
                 low[u] = min(low[v], low[u]);
 
                 if(low[v] > disc[u]){
                     bridges.push_back(vector<int> ({u, v}));
                 }
             }
-            else if(v != parent[u]){
+            else{
                 low[u] = min(low[u], disc[v]);
             }
         }
     }
 
     void findBridges_Tarjan(int V, vector<vector<int> > &bridges){
-        vector<int> disc(V, -1), low(V, -1), parent(V, -1);
+        vector<int> disc(V, -1), low(V, -1);
         // apply dfs for each component
         for(int i = 0; i < V; i++){
             if(disc[i] == -1){
-                DFS(i, disc, low, parent, bridges);
+                DFS(i, -1, disc, low, bridges);
             }
         }
     }

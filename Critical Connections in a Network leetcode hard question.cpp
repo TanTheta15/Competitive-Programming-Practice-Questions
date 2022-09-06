ca67a09db94942cccd9f756a90ef2957f1bbc51b
TanTheta15 <100551214+TanTheta15@users.noreply.github.com>
2022-09-06 18:49:34 +0530

    Update Critical Connections in a Network leetcode hard question.cpp

--- Critical Connections in a Network leetcode hard question.cpp
+++ Critical Connections in a Network leetcode hard question.cpp
@@ -1,3 +1,12 @@
+/*1192. Critical Connections in a Network.
+There are n servers numbered from 0 to n - 1 connected by undirected server-to-server connections forming a network where connections[i] = 
+[ai, bi] represents a connection between servers ai and bi. Any server can reach other servers directly or indirectly through the network.
+
+A critical connection is a connection that, if removed, will make some servers unable to reach some other server.
+
+Return all critical connections in the network in any order.*/
+
+
 unordered_map<int, vector<int> > adj;
 
     void DFS(int u, int parent, vector<int> &disc, vector<int> &low, vector<vector<int> > &bridges){
